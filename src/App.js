6ab0d8a0da8201725b686be2cd6e0e808735a9cc
daniel_luz <danisluz@gmail.com>
2022-07-07 23:59:56 -0300

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Saiba mais sobre o React
-        </a>
-      </header>
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+			<header className='banner'>
+        <img src='./images/banner.png' title='Banner home'/>
+			</header>
+    )
+}
+
+export default Banner;
